3481659b7884824f6f739d5813f67bfcda59e5b0
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-19 18:48:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -44,35 +44,8 @@ def insert_user(conn, username, email):
         user = get_user_by_username(connection, "JohnDoe")
         if user:
             print("User found:", user)
-        # Update user email
-        update_user_email(connection, "JohnDoe", "new_email@example.com")
-        # Read the updated user
-        updated_user = get_user_by_username(connection, "JohnDoe")
-        if updated_user:
-            print("Updated user:", updated_user)
-        # Delete the user
-        delete_user(connection, "JohnDoe")
-        # user with shortest email
-        insert_user(connection, "Apple", "a@example.com")
-        insert_user(connection, "Banana", "Banana@example.com")
-        insert_user(connection, "Orange", "ora@example.com")
-        person = select_person_with_shortest_email(connection)
-        if person:
-            print("Person with the shortest email:")
-            print("User ID:", person[0])
-            print("Username:", person[1])
-            print("Email:", person[2])
-        else:
-            print("No users in the database.")
-        # select all users
-        users = select_all_users(connection)
-        for user in users:
-            print("User ID:", user[0])
-            print("Username:", user[1])
-            print("Email:", user[2])
-
+  
         connection.close()
-
 
 if __name__ == "__main__":
     main()
