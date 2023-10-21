b65d0a8566663cba3f3eecf4a5f5042f509aaff8
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-10-21 13:16:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ setup(
         ],
     },
     install_requires=[
-    #devops
+    #devops,
     black==22.3.0,
     click==8.1.3,
     pytest==7.1.3,
     pytest-cov==4.0.0,
     pylint==2.15.3,
     boto3==1.24.87,
-    #web
+    #web,
     fastapi==0.85.0,
     uvicorn==0.18.3,
-    #data
+    #data,
     pandas == 2.1.0,
       ],
 )
